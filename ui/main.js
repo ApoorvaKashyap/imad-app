6cdb2357d481cfd790cfae577c1d70163f78ffaa
ApoorvaKashyap <30412013+ApoorvaKashyap@users.noreply.github.com>
2017-09-16 13:45:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,13 +20,29 @@ function clicked()
     request.send(null);
 }
 
-//var nameInput=document.getElementById('name').value;
+var nameInput=document.getElementById('name').value;
 var submit=document.getElementById('sub');
 submit.onclick = function(){
-    var names=['Jekyll','Mr. Hyde','Heidi','Cpt. Neemo'];
-    var list='';
-    for(var i=0;i<names.length;i++){
-        list+='<li>'+names[i]+'</li>';
-    }
-    document.getElementById('namelist').innerHTML=list;
+    
+    //Create a request object
+    var request = new XMLHttpRequest();
+    
+    //Take Action
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE)
+            if(request.status === 200)
+            {
+                var names=['Jekyll','Mr. Hyde','Heidi','Cpt. Neemo'];
+                var list='';
+                for(var i=0;i<names.length;i++){
+                list+='<li>'+names[i]+'</li>';
+                }
+                document.getElementById('namelist').innerHTML=list;
+            }
+    };
+    
+    //Make the request
+    request.open('GET','http://apoorvakashyap54.imad.hasura-app.io/submit_names?name='+nameInput,true);
+    request.send(null);
+    
 };
