f201642600e8d3ff28f22de4899d153e24be7414
ApoorvaKashyap <30412013+ApoorvaKashyap@users.noreply.github.com>
2017-09-17 12:22:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -152,8 +152,8 @@ app.get('/articles/:articlename',function(req,res){
             res.status(500).send(err.toString());
         else 
         {
-            if(result.rows.length == 0)
-                res.status(404).send('Article Not Found!')
+            if(result.rows.length === 0)
+                res.status(404).send('Article Not Found!');
             else
             {    
                 var articleData=result.rows[0];
