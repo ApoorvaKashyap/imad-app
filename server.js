1ca4b51c67f99874870ded7dcb39d72297b98aaa
ApoorvaKashyap <30412013+ApoorvaKashyap@users.noreply.github.com>
2017-09-17 11:17:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var pool = new Pool(config);
 var config = {
     user:'apoorvakashyap54',
     database:'apoorvakashyap54',
-    host:'http://db.imad.hasura-app.io/',
+    host:'db.imad.hasura-app.io',
     port:'5432',
     password:process.env.DB_Password
 };
